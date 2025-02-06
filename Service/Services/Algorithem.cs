5d295e23239df36c748158539069baed28dbc196
chanisiroka <60046354+chanisiroka@users.noreply.github.com>
2025-02-06 09:56:46 +0200

    Add files via upload

---
+++ Service/Services/Algorithem.cs
@@ -0,0 +1,35 @@
+﻿using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Services
+{
+    public class Algorithem : IAlgorithem
+    {
+        public async Task<string> getData()
+        {
+            
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            using (HttpClient client = new HttpClient())
+            {
+                //token
+              //  client.DefaultRequestHeaders.Add("Authorization", "Bearer ");
+                HttpResponseMessage response = await client.GetAsync(url);
+                try
+                {
+                    response.EnsureSuccessStatusCode(); //200
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    return responseBody;
+                }
+                catch (Exception ex)
+                {
+                    
+                    return "not success...";
+                }
+            }
+        }
+    }
+}
